d73dbbad313c3bd27c1e996ef7ec48a3bc9c48de
Christian Trutz <info@trutz-software-consulting.de>
2023-02-05 20:22:34 +0100

    chore: Projektrahmen

---
+++ src/main/java/trutz/io/viessmann/migration/filenet/ViessmannMigrationFilenetApplication.java
@@ -0,0 +1,13 @@
+package trutz.io.viessmann.migration.filenet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViessmannMigrationFilenetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViessmannMigrationFilenetApplication.class, args);
+	}
+
+}
