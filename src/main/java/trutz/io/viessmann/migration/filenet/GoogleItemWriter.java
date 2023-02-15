4825b68cec671cf208cd5ef277bb658ae90db227
Christian Trutz <info@trutz-software-consulting.de>
2023-02-15 12:03:19 +0100

    fix: Problem mit @NonNull behoben

--- src/main/java/trutz/io/viessmann/migration/filenet/GoogleItemWriter.java
+++ src/main/java/trutz/io/viessmann/migration/filenet/GoogleItemWriter.java
@@ -2,13 +2,14 @@ package trutz.io.viessmann.migration.filenet;
 
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GoogleItemWriter implements ItemWriter<FileNetDocument> {
 
 	@Override
-	public void write(Chunk<? extends FileNetDocument> chunk) throws Exception {
+	public void write(@NonNull Chunk<? extends FileNetDocument> chunk) throws Exception {
 		for (FileNetDocument item : chunk.getItems()) {
 			System.out.print(item);
 		}
